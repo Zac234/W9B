dd355a7c98d5acef0b48e7814159e5f77d914abf
Zach <zacbenincasa@gamil.com>
2023-03-16 20:03:20 -0600

    added in assignments w9a and w9b

---
+++ js/app.js
@@ -0,0 +1,28 @@
+let age=23
+let username=`steve`
+let is_subscribed= true
+
+
+if(age>=23 + is_subscribed==true){
+    console.log(`welcome user`)
+}else{
+    console.log(`you are not welcome`)
+}
+
+if(age<23+is_subscribed==true){
+    console.log(`you are too young`)
+}else{
+    comsole.log(`you are old enough to enter`)
+}
+
+if(username==`steve`+ is_subscribed==true){
+    console.log(`welcome steve`)
+}else{
+    console.log(`login with different username`)
+}
+
+if(username==null+age===23){
+    console.log(`choose a vaild username`)
+}else{
+    console.log(`welcome back user`)
+}
